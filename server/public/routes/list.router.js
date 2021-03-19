d2d650d21d6b6e066ec73f1e6f628fe71cf62340
Trace Brandt <trpbr92@gmail.com>
2021-03-19 16:04:06 -0500

    server is up

---
+++ server/public/routes/list.router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const listRouter = express.Router();
+
+
+
+
+module.exports = listRouter;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 5000;
+// const listRouter = require('./routes/list.router')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+//ROUTES
+// app.use('/list', listRouter)
+
+app.listen(port,()=>{
+    console.log('listening on port:', port);
+});
