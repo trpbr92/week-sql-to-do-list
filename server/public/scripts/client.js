709a8c1cf9ba15100fea2ac7077d64299d524767
Trace Brandt <trpbr92@gmail.com>
2021-03-20 08:55:46 -0500

    add task button created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,11 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('JQ');
+    //setup click handler
+$('#addButton').on('click', addTask);
 }//end onReady
+
+function addTask(){
+console.log('in addTask');
+
+}
