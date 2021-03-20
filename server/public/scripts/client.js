a0db14e5bad780a8d96ab2dbff6231eac1ea8f3d
Trace Brandt <trpbr92@gmail.com>
2021-03-20 12:16:29 -0500

    GET functioning and appending to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,7 +4,10 @@ function onReady(){
     console.log('JQ');
     //setup click handler
 $('#addButton').on('click', addTask);
+getList();
 }//end onReady
+
+
 
 function addTask(){
 console.log('in addTask');
@@ -20,9 +23,33 @@ $.ajax({
     url: '/list',
     data: listObject
 }).then(function(response){
-    console.log('back from post with:', response);
+    console.log('back from POST with:', response);
+    getList();
 }).catch(function(err){
     alert('error adding item to db');
     console.log(err);
 })
 }//end addTask
+
+
+function getList(){
+    console.log('in getList');
+    $.ajax({
+        method: 'GET',
+        url: '/list'
+    }).then(function(response){
+        console.log('back from GET:', response);
+        let el = $('#viewTasks');
+        el.empty();
+        for (let i = 0; i < response.length; i++) {
+            el.append(`
+        <tr>
+            <th>${response[i].task}</th>
+            <th>${response[i].complete}</th>
+        </tr>    
+            `)
+        }//end for
+    }).catch(function(error){
+        console.log('error in GET', error);
+    })
+}//end getList
--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 const pool = require ('../modules/pool');
 
+//GET
+router.get('/',(req, res)=>{
+    console.log('list route GET');
+    let queryString = `SELECT * FROM "list"`;
+    pool.query(queryString).then(results =>{
+        res.send(results.rows)
+    }).catch(error =>{
+        console.log('error getting list', error);
+        res.sendStatus(500);
+    })
+})
+
+
 //POST
 router.post('/',(req, res)=>{
-    let queryString = `INSERT INTO "list" ("task", "complete") VALUES ($1, $2)`;
+    let queryString = `INSERT INTO "list" (task, complete) VALUES ($1, $2)`;
     pool.query(queryString, [req.body.task, req.body.complete]).then((results)=>{
         res.sendStatus(200);
     }).catch((err)=>{
